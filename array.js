77c50077899379ac6364d2c0db716dd1511c9872
MOHAMMAD DAYLAKI <mohammad.daylaky@gmail.com>
2017-01-06 07:08:57 +0000

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,27 @@
 'use strict';
+function createArray() {
+  var array=["snickers", "hundred grand", "kitkat", "skittles"];
+  return array
+}
+function addElementToArray(array) {
+  array.push("A Christmas Story");
+  return array
+}
+function accessElementFromArray(array) {
+  return array[2]
+}
+function replaceElementInArray(names) {
+  return names[1]="Carter";
+}
+function removeElementFromArray(dishes) {
+  dishes.splice(2,2,"Roast Chicken");
+  return dishes;
+}
+
+function iterateArray(numbers) {
+  var newNums=[];
+numbers.forEach(function helper(number) {
+  newNums.push(number+5)
+})
+return newNums
+}
