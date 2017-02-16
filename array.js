2ad3a2301fc55337d4b22f8a16b970c7b9296f96
fatmaMY <fatima.mohammad736@gmail.com>
2017-02-17 01:58:47 +0300

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,33 @@
 'use strict';
+function createArray() {
+	var stringArray = ["snickers", "hundred grand", "kitkat", "skittles"];
+	return stringArray;
+}
+
+function addElementToArray(array) {
+	array.push("A Christmas Story");
+	return array;
+}
+
+function accessElementFromArray(states) {
+	return states[2];
+}
+
+function replaceElementInArray(names) {
+	names[1] = "Carter";
+	return names[1];
+}
+
+function removeElementFromArray(dishes) {
+	dishes.splice(2, 2, "Roast Chicken");
+	return dishes;
+}
+
+function iterateArray(numbers) {
+	var newNums =[];
+	 numbers.forEach(function(num) {
+    newNums.push(num + 5);
+  });
+  return newNums;
+
+}
