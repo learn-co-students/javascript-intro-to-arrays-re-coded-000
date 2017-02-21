94abcbc1ba6aa97e5856d6704433282ee0ebc030
Noven Temer <noven5823@gmail.com>
2017-02-21 23:27:11 +0300

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,28 @@
 'use strict';
+function createArray() {
+    var random = new Array();
+      return random = ["snickers", "hundred grand", "kitkat", "skittles"];
+}
+function replaceElementInArray(names) {
+  names[1] = "Carter";
+  return names[1];
+}
+
+function addElementToArray(practice) {
+  practice.push("A Christmas Story");
+  return practice;
+}
+function accessElementFromArray(item) {
+  return item[2];
+}
+function removeElementFromArray(dishes) {
+  dishes.splice(2, 2, "Roast Chicken");
+  return dishes;
+}
+function iterateArray(numbers) {
+  var num = new Array();
+  numbers.forEach(function(number) {
+      num.push( number + 5 );
+  });
+  return num;
+};
