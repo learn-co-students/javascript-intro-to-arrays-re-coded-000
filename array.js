097218d1ee0d9daf6624fa4f585c418b051b1703
dlkhwaz <dllaomer93@gmail.com>
2017-02-21 21:37:30 +0300

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function createArray(){
+  return ["snickers", "hundred grand", "kitkat", "skittles"];
+
+}
+
+function addElementToArray(array){
+	array.push("A Christmas Story");
+	return array;
+}
+
+function accessElementFromArray(array){
+	return array[2];
+}
+
+function replaceElementInArray(array){
+  array[1]="Carter";
+	return array[1];
+}
+
+function removeElementFromArray(dishesArray){
+	dishesArray.splice(2,3,"Roast Chicken");
+	return dishesArray
+}
+
+function iterateArray(numArr){
+  var newNums = [];
+  numArr.forEach(function(num) {
+    newNums.push(num + 5);
+  });
+  return newNums;
+}
