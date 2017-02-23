8183fe71d030414dd6477922efe0a2372774e680
Omar Suliman <omarsulyman@gmail.com>
2017-02-23 17:37:36 +0300

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,33 @@
 'use strict';
+function createArray() {
+	return ["snickers", "hundred grand", "kitkat", "skittles"];
+}
+
+function addElementToArray(arr) {
+	arr.push('A Christmas Story');
+	return arr;
+}
+
+function accessElementFromArray(q) {
+	return q[2];
+}
+
+function replaceElementInArray(q) {
+	// body...
+	q[1]="Carter";
+	return q[1];
+}
+
+function removeElementFromArray(dishesArr){
+  dishesArr.splice(2, 2, "Roast Chicken");
+  return dishesArr;
+}
+
+function iterateArray(arr) {
+	var newarray=[];
+	arr.forEach(function(item){
+		newarray.push(item +5);
+	});
+	return newarray;
+
+}
