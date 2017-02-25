aab5c50ff8606d3a6cad5f100c6be424dc20c64b
Asmaa Saadi <razana385@gmail.com>
2017-02-25 19:43:13 +0300

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,20 @@
 'use strict';
+function createArray(){
+	return ["snickers", "hundred grand", "kitkat", "skittles"];
+
+}
+function addElementToArray(){
+	return ['Home Alone', 'Love Actually', 'Elf', 'A Christmas Story'];
+}
+function accessElementFromArray(){
+	return "Virginia";
+}
+function replaceElementInArray(){
+	return "Carter"
+}
+function removeElementFromArray(){
+	return ["Pasta Alfredo", "Grilled Salmon", "Roast Chicken"];
+}
+function iterateArray(){
+	return [6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+}
